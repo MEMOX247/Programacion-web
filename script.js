414c5811874cf522a9d4a6664a536f6da57390e2
MEMOX247 <acastagnino@itba.edu.ar>
2023-06-27 23:44:10 -0300

    Entrega Final Programacion Web

--- script.js
+++ script.js
@@ -1,53 +1,4 @@
 
-// // Variables globales
-// let carrito = [];
-// let total = 0;
-
-// // Función para agregar un producto al carrito
-// function agregarAlCarrito(nombre, precio) {
-//   carrito.push({ nombre, precio });
-//   total += precio;
-  
-//   actualizarCarrito();
-// }
-
-// // Función para actualizar el contenido del carrito en el HTML
-// function actualizarCarrito() {
-//   const itemsCarrito = document.getElementById('items-carrito');
-//   const totalCarrito = document.getElementById('total-carrito');
-
-//   // Limpiar el contenido previo del carrito
-//   itemsCarrito.innerHTML = '';
-  
-//   // Actualizar los elementos del carrito
-//   carrito.forEach((producto, index) => {
-//     const li = document.createElement('li');
-//     li.innerText = `${producto.nombre} - $${producto.precio}`;
-    
-//     const botonEliminar = document.createElement('button');
-//     botonEliminar.innerText = ' - ';
-//     botonEliminar.addEventListener('click', () => {
-//       eliminarDelCarrito(index);
-//     });
-    
-//     li.appendChild(botonEliminar);
-//     itemsCarrito.appendChild(li);
-//   });
-
-
-
-//   // Actualizar el total del carrito
-//   totalCarrito.innerText = total;
-// }
-
-// function eliminarDelCarrito(index) {
-//   const productoEliminado = carrito.splice(index, 1)[0];
-//   total -= productoEliminado.precio;
-    
-//   actualizarCarrito();
-// }
-
-
 // Variables globales
 let carrito = [];
 let total = 0;
@@ -124,4 +75,25 @@ function obtenerCarritoDeLocalStorage() {
 
 // Llamar a la función para obtener el carrito desde localStorage al cargar la página
 window.addEventListener('load', obtenerCarritoDeLocalStorage);
+
+// Variables globales
+let index = 0; // Índice de la imagen actual
+const intervaloTiempo = 3000; // Intervalo de tiempo entre las transiciones (en milisegundos)
+
+const carrusel = document.getElementById('carrusel');
+const imagenes = Array.from(carrusel.getElementsByClassName('imagen'));
+let indice = 0;
+
+function mostrarSiguienteImagen() {
+  imagenes[indice].classList.remove('activo');
+  indice = (indice + 1) % imagenes.length;
+  imagenes[indice].classList.add('activo');
+}
+
+// Muestra la primera imagen por defecto
+imagenes[indice].classList.add('activo');
+
+// Configura el intervalo para mostrar las imágenes automáticamente
+setInterval(mostrarSiguienteImagen, 3000); // Cambia la imagen cada 3 segundos (ajusta el tiempo según tus necesidades)
+
 
